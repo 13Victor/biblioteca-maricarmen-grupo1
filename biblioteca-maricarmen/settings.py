70a3593c1f2ec69f3a15fd4b42cb33638df4450c
Sergio <sergio.fdce@gmail.com>
2025-04-25 17:03:07 +0200

    [FIX] Error 403 en /admin

--- biblioteca-maricarmen/settings.py
+++ biblioteca-maricarmen/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'biblioteca.middleware.AdminAccessMiddleware',  # Añadir nuestro middleware al final
 ]
 
 CORS_ALLOWED_ORIGINS = [
